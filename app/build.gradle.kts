4ad1d5ed3b0a27c00850962bb9609b757211426a
xin.xiao <xin.xiao@udschina.com>
2026-07-03 17:01:22 +0800

    chore: 打包

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.example.pinkschedule"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
